c8995225f44126e46e944d84800efcbf4e5b0437
mikey1 <mikey1997@gmail.com>
2023-02-17 09:50:32 +0530

    Added modules provider

---
+++ main.tf
@@ -0,0 +1,15 @@
+module "vpc-net" {
+    source = "github.com/mikey-19961997/MODULE/modules/vpc-net"
+    vpc_details = {
+        Name = "mikey-vpc"
+        cidr_block = "192.168.0.0/16"
+    }
+
+    subnet_details = {
+        Name = ["m1","m2","m3","m4"]
+    }
+}
+
+output "subnetsno" {
+    value = module.vpc-net.subnetcount
+}
